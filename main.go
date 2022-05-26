3c915da2f5d59c8827f0cfcc378892c2a73e9e7e
CodeMichelle <michelle.taylor@parchment.com>
2022-05-25 21:50:03 -0500

    Deeper into Go - byte slices, multiple return values, range, etc.

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
 	cards := newDeck()
-	cards.print()
+	cards.toString()
+	fmt.Println(cards.toString())
 }
